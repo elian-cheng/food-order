d45d732b5d36cd0a13f994b3efe2bbe986f3cd34
Olga Chernega <eliang.cheng@gmail.com>
2023-05-26 15:49:42 +0300

    feat: add server

--- frontend/src/API/URL.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const BASE_URL = window.location.origin.includes('local')
-  ? 'http://localhost:5000/'
-  : 'https://netlify';
-
-axios.defaults.baseURL = BASE_URL;
